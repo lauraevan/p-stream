6499b09a3f3d08593c1c2c841c1ff41c43db9f64
Polaris <264532369+lauraevan@users.noreply.github.com>
2026-09-01 00:43:42 -0500

    simplify bottom player controls

--- src/components/player/base/BottomControls.tsx
+++ src/components/player/base/BottomControls.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 
 import { Transition } from "@/components/utils/Transition";
 import { usePlayerStore } from "@/stores/player/store";
-import { useSubtitleStore } from "@/stores/subtitles";
 
 export function BottomControls(props: {
   show?: boolean;
@@ -10,9 +9,6 @@ export function BottomControls(props: {
 }) {
   const setHoveringAnyControls = usePlayerStore(
     (s) => s.setHoveringAnyControls,
-  );
-  const backgroundBlurEnabled = useSubtitleStore(
-    (s) => s.styling.backgroundBlurEnabled,
   );
 
   useEffect(() => {
@@ -23,17 +19,10 @@ export function BottomControls(props: {
 
   return (
     <div className="w-full text-white">
-      {backgroundBlurEnabled && (
-        <Transition
-          animation="fade"
-          show={props.show}
-          className="pointer-events-none flex justify-end pt-32 bg-gradient-to-t from-black to-transparent transition-opacity duration-200 absolute bottom-0 w-full"
-        />
-      )}
       <div
         onMouseOver={() => setHoveringAnyControls(true)}
         onMouseOut={() => setHoveringAnyControls(false)}
-        className="pointer-events-auto z-10 pl-[calc(2rem+env(safe-area-inset-left))] pr-[calc(2rem+env(safe-area-inset-right))] pb-3 mb-[env(safe-area-inset-bottom)] absolute bottom-0 w-full"
+        className="pointer-events-auto absolute bottom-0 z-20 w-full px-[calc(0.75rem+env(safe-area-inset-left))] pb-[calc(0.75rem+env(safe-area-inset-bottom))] pr-[calc(0.75rem+env(safe-area-inset-right))] sm:px-[calc(1.25rem+env(safe-area-inset-left))] sm:pb-[calc(1.25rem+env(safe-area-inset-bottom))] sm:pr-[calc(1.25rem+env(safe-area-inset-right))]"
       >
         <Transition animation="slide-up" show={props.show}>
           {props.children}
